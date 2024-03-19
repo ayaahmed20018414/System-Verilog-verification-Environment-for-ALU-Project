f543c58bde7adb6eeb0fddf0d37c18852414a276
Aya Ahmed <82789012+ayaahmed20018414@users.noreply.github.com>
2024-03-19 21:53:52 +0200

    Add files via upload

--- ALU Design/design.sv
+++ ALU Design/design.sv
@@ -1,32 +1,81 @@
 // Code your design here
-module alu(alu_interface.dut intf);
-  logic [8:0] result_temp;
-  always @(posedge intf.clk or posedge intf.reset) begin
-    if(intf.reset) begin
-      intf.result<='b0;
-      intf.carry<=1'b0;
+module ALU_design (alu_interface.dut intf);
+  always @(posedge intf.clk or negedge intf.rst_n) begin
+    if(!intf.rst_n) begin
+      intf.C<='b0;
+    end
+    else if(intf.ALU_en) begin
+      if(intf.a_en && !intf.b_en) begin
+        case(intf.a_op)
+          3'b000:begin
+            intf.C<=intf.A + intf.B;
+          end
+          3'b001:begin
+            intf.C<=intf.A - intf.B;
+          end
+          3'b010:begin
+            intf.C<=intf.A ^ intf.B;
+          end
+          3'b011:begin
+            intf.C<=intf.A  & intf.B;
+          end
+          3'b100:begin
+            intf.C<=intf.A  & intf.B;
+          end
+          3'b101:begin
+            intf.C<=intf.A  | intf.B;
+          end
+          3'b110:begin
+            intf.C<=~(intf.A  ^ intf.B);
+          end
+          3'b111:begin
+            $display("ERROR you aren't allowed to Enter this value Enter from 0 to 6 only");
+          end   
+        endcase
+        
+      end
+      else if(!intf.a_en && intf.b_en) begin
+        case(intf.b_op)
+          2'b00:begin
+            intf.C<=~(intf.A  & intf.B);
+          end
+          2'b01:begin
+            intf.C<=intf.A  + intf.B;
+          end
+          2'b10:begin
+            intf.C<=intf.A  + intf.B;
+          end
+          2'b11:begin
+            $display("ERROR you aren't allowed to Enter this value Enter from 0 to 2 only");
+          end
+        endcase
+      end
+      else if(intf.a_en && intf.b_en) begin
+       case(intf.b_op)
+          2'b00:begin
+            intf.C<=intf.A  ^ intf.B;
+          end
+          2'b01:begin
+            intf.C<=~(intf.A  ^ intf.B);
+          end
+          2'b10:begin
+            intf.C<=intf.A  - 1;
+          end
+          2'b11:begin
+            intf.C<=intf.B  + 2;
+          end
+        endcase
+      end
+      else begin
+        intf.C<='b0;
+      end
     end
     else begin
-      {intf.carry,intf.result} <=result_temp;
-    end
+      intf.C<='b0;
+    end     
   end
-    always @(*) begin
-      case(intf.op_code)
-        4'b0000:begin
-          result_temp<=intf.a+intf.b;
-        end
-       4'b0001:begin
-         result_temp<=intf.a-intf.b;
-        end
-       4'b0010:begin
-         result_temp<=intf.a*intf.b;
-        end
-        4'b0011:begin
-          result_temp<=intf.a/intf.b;
-        end
-        
-      endcase
-  end
+  
+  
   
   
 endmodule
